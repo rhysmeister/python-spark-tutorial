79460493ad47c481930c31d933955167764961c0
Rhys Campbell <rhys.james.campbell@googlemail.com>
2018-08-08 15:02:39 +0200

    Correct off by one

--- rdd/airports/AirportsByLatitudeSolution.py
+++ rdd/airports/AirportsByLatitudeSolution.py
@@ -5,16 +5,16 @@ from commons.Utils import Utils
 
 def splitComma(line: str):
     splits = Utils.COMMA_DELIMITER.split(line)
-    return "{}, {}".format(splits[1], splits[6])
+    return "{}, {}".format(splits[1], splits[7])
 
 if __name__ == "__main__":
     conf = SparkConf().setAppName("airports").setMaster("local[*]")
     sc = SparkContext(conf = conf)
-    
+
     airports = sc.textFile("in/airports.text")
 
-    airportsInUSA = airports.filter(lambda line: float(Utils.COMMA_DELIMITER.split(line)[6]) > 40)
-    
+    airportsInUSA = airports.filter(lambda line: float(Utils.COMMA_DELIMITER.split(line)[7]) > 40)
+
     airportsNameAndCityNames = airportsInUSA.map(splitComma)
 
     airportsNameAndCityNames.saveAsTextFile("out/airports_by_latitude.text")
